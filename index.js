467a08895637cc627eb1acf7e8c70edff8279844
SergeyKosyanyuk <ser.ser.05021997@gmail.com>
2019-02-27 15:50:30 +0300

    fix bug

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
+const cont = document.getElementsByClassName('test')[0];
 
 // replace the uri string with your connection string.
 const uri = "mongodb+srv://sergey_admin_22:5VAs8wkFx5fXTrr@cluster0-whxsz.gcp.mongodb.net/test?retryWrites=true"
 MongoClient.connect(uri, function(err, client) {
    if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+        //console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+        cont.innerHTML = err;
    }
-   console.log('Connected...');
+   //console.log('Connected...');
+   
+   cont.innerHTML = "Connected...";
    const collection = client.db("test").collection("devices");
    // perform actions on the collection object
    client.close();
